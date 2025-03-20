682d5991a7bec5bf4bb6bba3706ed596bdd85633
Jacob Jørgensen <me@jacob-j.com>
2025-03-20 09:31:01 +0100

    chore: remove old background setup code

--- installer/install.sh
+++ installer/install.sh
@@ -4,16 +4,5 @@
 sudo pacman -Syy
 sudo pacman -S --noconfirm $(cat ./installer/dependencies.txt)
 
-# Set background
-dbus-send --session --dest=org.kde.plasmashell --type=method_call \
-/PlasmaShell org.kde.PlasmaShell.evaluateScript 'string:
-var Desktops = desktops();
-for (i=0;i<Desktops.length;i++) {
-        d = Desktops[i];
-        d.wallpaperPlugin = "org.kde.image";
-        d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");
-        d.writeConfig("Image", "file://home/jacob/dotfiles/wallpapers/background.jpg");
-}'
-
 # Set up symlinks.
 stow --adopt .
